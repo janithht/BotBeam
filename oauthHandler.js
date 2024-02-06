6d2fd2aa9aeeae9a286704fc10dcf4c7faa323b1
janithht <hathnagodahwemjn.20@uom.lk>
2024-02-06 10:01:04 +0530

    installation page redirect

--- oauthHandler.js
+++ oauthHandler.js
@@ -6,7 +6,6 @@ import open from 'open';
 import dotenv from 'dotenv';
 
 dotenv.config();
-
 const clientId = "Iv1.8891ec172d49e757";
 const clientSecret = "102a007a9c00abd57cf0f4daec9bd7b97d10dfd2";
 
@@ -15,6 +14,7 @@ const handleOAuth = async() => {
     // Start a temporary server to handle the OAuth callback
     const app = express();
     const server = app.listen(3000, () => {
+        
         console.log('Temporary server running on http://localhost:3000');
         open(`https://github.com/login/oauth/authorize?client_id=${clientId}&scope=repo`).then(() => {
             console.log('Browser opened successfully');
@@ -38,14 +38,13 @@ const handleOAuth = async() => {
 
             const accessToken = response.data.access_token;
             console.log('Access Token:', accessToken);
-            res.send('Authentication successful! You can close this window.');
 
-            // Redirect or instruct the user to install the GitHub App
-            console.log(`Please install the GitHub App using this link: https://github.com/apps/your-app-name/installations/new`);
-        } catch (error) {
+            const appInstallationUrl = `https://github.com/apps/codecommentor/installations/new`;
+            res.redirect(appInstallationUrl);
+            } catch (error) {
             console.error('Error exchanging code for token', error);
             res.send('Authentication failed');
-        } finally {
+            } finally {
             server.close();
         }
     });
