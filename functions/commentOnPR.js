179c9db2b2f047190add68a6d14e7506a3a65c70
janithht <hathnagodahwemjn.20@uom.lk>
2024-02-19 14:37:24 +0530

    Labels added

--- functions/commentOnPR.js
+++ functions/commentOnPR.js
@@ -6,26 +6,38 @@ dotenv.config();
 async function addComment(owner, repo, issue_number, body) {
   try {
     const installationId = await getInstallationId(owner, repo);
-    if(!installationId) throw new Error('Failed to get installation ID');
+    if (!installationId) {
+      console.error('Failed to get installation ID');
+      return;
+    }
     
-      const octokit = new Octokit({
-          authStrategy: createAppAuth,
-          auth: {
-              appId: process.env.GITHUB_APP_ID,
-              privateKey: process.env.GITHUB_PRIVATE_KEY.replace(/\\n/g, '\n'),
-              installationId: installationId,
-          },
-      });
+    const octokit = new Octokit({
+      authStrategy: createAppAuth,
+      auth: {
+        appId: process.env.GITHUB_APP_ID,
+        privateKey: process.env.GITHUB_PRIVATE_KEY.replace(/\\n/g, '\n'),
+        installationId: installationId,
+      },
+    });
 
-      await octokit.rest.issues.createComment({
-          owner: owner,
-          repo: repo,
-          issue_number: issue_number,
-          body: body,
-      });
-      return{status: 'success', message: 'Comment added to PR'};
+    await octokit.rest.issues.createComment({
+      owner: owner,
+      repo: repo,
+      issue_number: issue_number,
+      body: body,
+    });
+
+    // Adding labels to the PR/Issue
+    await octokit.rest.issues.addLabels({
+      owner: owner,
+      repo: repo,
+      issue_number: issue_number,
+      labels: ['BuildFailure'] 
+    });
+
+    return { status: 'success', message: 'Comment added to PR' };
   } catch (error) {
-      throw new error('Failed to add comment:', error);
+      throw new Error(`Failed to add comment: ${error.message}`);
   }
 }
 
